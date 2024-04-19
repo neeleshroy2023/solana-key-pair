deaebf7543b3818a01db71f7456e5fc44a1cb2d6
neeleshroy2023 <neelesh.roy@layrins.com>
2024-04-19 12:29:02 +0530

    key pair generated with env

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,6 +1,11 @@
-import { Keypair } from "@solana/web3.js";
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
-const keypair = Keypair.generate();
+const keypairFromEnv = getKeypairFromEnvironment("SOLANA_SECRET_KEY");
 
-console.log(keypair.secretKey);
-console.log(keypair.publicKey.toBase58());
+if (!keypairFromEnv) {
+  console.error("No keypair found in environment");
+  process.exit(1);
+}
+
+console.log("Loaded keypair from environment ✅");
